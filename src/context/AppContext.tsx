0f5f553846ed96d3a91239da09282a2083b631c2
Jack Padalino <pada0867@gmail.com>
2025-11-19 13:27:11 -0500

    added new context slice

---
+++ src/context/AppContext.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react';
+
+const AppContextProvider: FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  return (
+
+                          {children}
+                    
+  
+  );
+};
+
+export default AppContextProvider;
---
+++ src/context/UserContext.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import React, {
+  createContext,
+  useState,
+  ReactNode,
+  Dispatch,
+  SetStateAction,
+  FC,
+} from 'react';
+import { Profile } from '@/types';
+
+// defining types for context and provider
+export type UserContextType = {
+  // user information
+  signedIn: boolean;
+  setSignedIn: Dispatch<SetStateAction<boolean>>;
+  // profile
+  profile: Profile | undefined;
+  setProfile: Dispatch<SetStateAction<Profile | undefined>>;
+  // reset all context state
+  resetUserContext: () => void;
+};
+
+export type UserContextProviderProps = {
+  children: ReactNode;
+};
+
+// create the context object
+export const UserContext = createContext<UserContextType | []>([]);
+
+// create the provider to pass props to components
+export const UserContextProvider: FC<UserContextProviderProps> = ({
+  children,
+}) => {
+  // user information
+  const [signedIn, setSignedIn] = useState<boolean>(false);
+  // profile
+  const [profile, setProfile] = useState<Profile | undefined>(
+    undefined
+  );
+  
+
+  const resetUserContext = () => {
+    setSignedIn(false);
+    setProfile(undefined);
+  };
+
+  // values and functions that will be provided to our app's components
+  const value: UserContextType = {
+    signedIn,
+    setSignedIn,
+    profile,
+    setProfile,
+    resetUserContext,
+  };
+
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+};
