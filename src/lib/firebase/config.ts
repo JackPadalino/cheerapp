d5c8650cb7d3aaee5c24ec3758528d06f7118aa7
Jack Padalino <pada0867@gmail.com>
2025-11-19 11:36:58 -0500

    added auth to config file

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -1,5 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -14,5 +16,9 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app)
 
-export { app, db, initializeApp };
+export {
+  db,
+  auth, 
+};
